6732fbd2d9575971b1d4c3aa127de53ae3bd2747
ShikharBansal <shikharbansal007@gmail.com>
2021-08-28 15:42:01 +0530

    Done with landing page and navbar components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,17 @@
+import React from "react";
 import "./App.css";
+import Landing from "./Landing";
+import Nav from "./Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Shikhar</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Nav/>
+        <Landing/>
+      </div>
+    );
+  }
 }
 
 export default App;
